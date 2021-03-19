9fb73732b9ac555f74e01a2338cbfc5304cecfdc
h4nvotex <laurence.hupp@gmail.com>
2021-03-19 12:07:34 +0100

    aufgabe 2

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(moveHorizontal * speed, 0, moveVertical * speed, ForceMode.Force);
+    }
+}
