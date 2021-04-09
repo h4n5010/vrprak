ee124753845ba8e6eddfb3d45fc215ad703eb707
h4nvotex <laurence.hupp@gmail.com>
2021-04-09 16:21:14 +0200

    added hud

---
+++ Assets/Scripts/DisplayFlightData.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DisplayFlightData : MonoBehaviour
+{
+    public Text textSpeed;
+    public Text textHeight;
+    public GameObject player;
+    public float warningHeight;
+    public GameObject warnImage;
+    private FlyJoystick flyJoystick;
+    private bool below_limit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flyJoystick = player.GetComponent<FlyJoystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float height = player.transform.position.y - (float)1.57;
+        textHeight.text = "Alt: " + height.ToString("F2") + "m";
+        textSpeed.text = "Speed: " + (300 * flyJoystick.speed).ToString("F2") + " km/h";
+        if (height < warningHeight)
+        {
+            if (!below_limit)
+            {
+                below_limit = true;
+                textHeight.color = new Color(255, 0, 0, 1);
+                StartBlinking();
+            }
+
+        }
+        else
+        {
+            if (below_limit)
+            {
+                below_limit = false;
+                textHeight.color = new Color(0, 0, 0, 1);
+                warnImage.SetActive(false);
+                StopBlinking();
+            }
+        }
+    }
+
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            switch (warnImage.activeSelf)
+            {
+                case false:
+                    warnImage.SetActive(true);
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+                case true:
+                    warnImage.SetActive(false);
+                    yield return new WaitForSeconds(0.5f);
+                    break;
+            }
+        }
+    }
+
+    void StartBlinking()
+    {
+        StopAllCoroutines();
+        StartCoroutine("Blink");
+    }
+
+    void StopBlinking()
+    {
+        StopAllCoroutines();
+    }
+}
---
+++ Assets/Scripts/FacePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+    public GameObject player;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.transform.position + distance * player.transform.forward;
+        this.transform.LookAt(player.transform.position);
+    }
+}
