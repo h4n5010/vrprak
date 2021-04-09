db17651ae0d0837fd475547758ece7a5500246a6
h4nvotex <laurence.hupp@gmail.com>
2021-04-09 12:12:26 +0200

    Initial flightsim

---
+++ Assets/Scripts/FollowPhysics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPhysics : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(target.transform.position);
+    }
+}
