b670a9bdc158f79959f541c7432bce55e52a5d16
h4nvotex <laurence.hupp@gmail.com>
2021-04-09 12:40:18 +0200

    added rotation to rotors

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SwitchScene : MonoBehaviour
+{
+    public string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.rotation.y < 0.7035)
+        {
+            Debug.Log("CHANGE" + this.transform.rotation.y);
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
