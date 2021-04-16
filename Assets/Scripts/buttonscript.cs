07e1adf8311a55643f953a3a77ed71e78958c287
Student <student@g006-01.rz.fh-ingolstadt.de>
2021-04-16 11:15:53 +0200

    aufgabe 2

---
+++ Assets/Scripts/buttonscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonscript : MonoBehaviour
+{
+
+    public AudioClip click;
+    public GameObject hud;
+    private bool enabled = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        enabled = !enabled;
+        if (!enabled)
+        {
+            this.transform.position += new Vector3(0.1f, 0, 0);
+            hud.SetActive(false);
+        } else
+        {
+            this.transform.position += new Vector3(-0.1f, 0, 0);
+            hud.SetActive(true);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # FlightSim
 
-HUD funktioniert
+## Zusatzfunktionen
+- wenn der Zylinder gedrückt wird wird das Hud an oder aus geschaltet
+- drei Helikopter starten selbständig und drehen sich um 90 Grad und fliegen dann geradeaus
+- eine Reihe von Militär Transporter fahren die Startbahn entlang und halten am Ende
